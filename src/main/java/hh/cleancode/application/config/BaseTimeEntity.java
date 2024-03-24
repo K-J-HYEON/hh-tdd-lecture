2dc608995b207de85d12a1cfb060753e291337dd
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-03-24 22:59:05 +0900

    초기 설정

--- src/main/java/hh/cleancode/application/config/BaseTimeEntity.java
+++ src/main/java/hh/cleancode/application/config/BaseTimeEntity.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createAt;
