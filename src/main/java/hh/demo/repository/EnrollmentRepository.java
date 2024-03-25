2ac4eb36e6e622452c07d4a748e8821b86b771f3
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-03-25 20:28:44 +0900

    Repository 코드 수정

--- src/main/java/hh/demo/repository/EnrollmentRepository.java
+++ src/main/java/hh/demo/repository/EnrollmentRepository.java
@@ -2,13 +2,17 @@ package hh.demo.repository;
 
 import hh.demo.domain.Enrollment;
 import hh.demo.domain.EnrollmentId;
+import hh.demo.domain.EnrollmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentId> {
 
-    Optional<Enrollment> findByIdLectureIdUserId(String lectureId, String userId);
+    Optional<Enrollment> findByIdLectureIdAndUserId(String lectureId, String userId);
+
+    List<Enrollment> findbyIdLectureIdAndUserIdAndStatus(String lectureId, String userId, EnrollmentStatus status);
 }
--- src/main/java/hh/demo/repository/LectureRepository.java
+++ src/main/java/hh/demo/repository/LectureRepository.java
@@ -1,10 +1,14 @@
 package hh.demo.repository;
 
 import hh.demo.domain.Lecture;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+public interface LectureRepository extends JpaRepository<Lecture, String> {
 
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-    Optional<Lecture> findByIdLecutrIdUserId(String lectureId, String userId);
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select lect from Lecture lec where l.id = :id")
+    Lecture findWithLockById(String id);
 }
