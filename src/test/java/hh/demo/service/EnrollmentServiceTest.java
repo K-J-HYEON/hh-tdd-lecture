e6a3ff8b78178526497bd0a47109bea1f4699c08
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-03-28 17:44:54 +0900

    EnrollmentServiceTest 수정

--- src/test/java/hh/demo/service/EnrollmentServiceTest.java
+++ src/test/java/hh/demo/service/EnrollmentServiceTest.java
@@ -1,10 +1,10 @@
 package hh.demo.service;
 
-import hh.demo.TestException;
 import hh.demo.domain.Enrollment;
 import hh.demo.domain.EnrollmentStatus;
 import hh.demo.dto.request.EnrollLectureReq;
 import hh.demo.repository.EnrollmentRepository;
+import hh.demo.repository.LectureRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,16 +12,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
-import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 /*
  * 특강 신청 테스트
  * 1. 특강 신청 성공
- * 2. 다른 신청자가 이미 특강을 신청한 경우
+ * 2. 같은 사용자 동일한 특강 중복 신청 불가
  * 3. 특강 신청이 30명 초과됐을 경우
  * */
+
 
 @ExtendWith(MockitoExtension.class)
 class EnrollmentServiceTest {
@@ -29,6 +30,7 @@ class EnrollmentServiceTest {
     EnrollmentService sut;
     @Mock
     EnrollmentRepository enrollmentRepository;
+    LectureRepository lectureRepository;
 
 
 //    1. 특강 신청 성공
@@ -48,22 +50,21 @@ class EnrollmentServiceTest {
         assertThat(enrollment.equals(lectureId)).isEqualTo(lectureId);
     }
 
-
-
-    // 다른 신청자가 이미 특강을 신청한 경우
     @Test
-    @DisplayName("다른 신청자가 이미 특강을 신청한 경우")
+    @DisplayName("같은 사용자 동일한 특강 중복 신청 불가")
     void EnrollmentTest2() throws Exception {
-        // Given
+
+        // given
         String userId = "test1";
         String lectureId = "test2";
-        EnrollLectureReq req = new EnrollLectureReq(userId, lectureId);
-        sut.existEnrollByLectureIdAndUserId(userId, lectureId);
+        EnrollmentStatus status = EnrollmentStatus.ENROLL_FAIL;
 
-        // when ** then
-        assertThrows(RuntimeException.class, () -> {
-            enrollmentRepository.findByIdLectureIdAndUserId(userId, lectureId);
-        });
+        // when
+        Optional<Enrollment> enrollment = sut.saveEnrollmentStatus(userId, lectureId, status);
+
+        // then
+        assertThat(enrollment.equals(lectureId)).isEqualTo(lectureId);
+
     }
 
     @Test
@@ -71,7 +72,7 @@ class EnrollmentServiceTest {
     void EnrollmentTest3() throws Exception {
         String userId = "test1";
         String lectureId = "test2";
-        EnrollLectureReq req = new EnrollLectureReq(userId, lectureId);
+        EnrollmentStatus status = EnrollmentStatus.ENROLL_SUCCESS;
 
         for (long i = 0; i < 30; i++) {
             enrollmentRepository.findByIdLectureIdAndUserId(lectureId, userId);
@@ -80,5 +81,50 @@ class EnrollmentServiceTest {
         assertThrows(RuntimeException.class, () -> {
             sut.existEnrollByLectureIdAndUserId(userId, lectureId);
         });
+    }
+
+     /*
+     * 1. 특강 신청 성공한 사용자는 성공했음을 리턴
+     * 2,.특강 신청 실패한 사용자는 실패했음을 리턴
+     */
+
+    @Test
+    @DisplayName("특강 신청 성공한 사용자는 성공했다 리턴")
+    void EnrollmentTest4() throws Exception {
+        // given
+        String userId = "test1";
+        String lectureId = "test2";
+        EnrollmentStatus status = EnrollmentStatus.ENROLL_SUCCESS;
+
+        // when
+        // 수정해야함
+        when(enrollmentRepository.findByIdLectureIdAndUserId(userId, lectureId)).thenReturn(Optional.ofNullable(lectureId));
+        when(lectureRepository.findWithLockById(lectureId)).thenReturn(true);
+        String enrollmentStatus = String.valueOf(sut.existEnrollByLectureIdAndUserId(userId, lectureId));
+
+        // then
+        assertThat(enrollmentStatus.equals("신청이 완료되었습니다."));
+    }
+
+
+    @Test
+    @DisplayName("특강 신청 실패한 사용자는 실패했다 리턴")
+    void EnrollmentTest5() throws Exception {
+
+        // given
+        String userId = "test1";
+        String lectureId = "test2";
+        EnrollmentStatus status = EnrollmentStatus.ENROLL_SUCCESS;
+
+        // when
+        // 수정해야함
+        when(enrollmentRepository.findByIdLectureIdAndUserId(userId, lectureId)).thenReturn(Optional.ofNullable(lectureId));
+        when(lectureRepository.findWithLockById(lectureId)).thenReturn(false);
+        String enrollmentStatus = String.valueOf(sut.existEnrollByLectureIdAndUserId(userId, lectureId));
+
+        // then
+        assertThat(enrollmentStatus.equals("신청이 완료되었습니다."));
+
+
     }
 }
